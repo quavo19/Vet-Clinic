a08dbcbfa92adf82c0a1c439c55278a9730ffb0c
Donald Akite <donaldakite27@gmail.com>
2023-02-21 14:24:42 +0000

    Update table name

--- queries.sql
+++ queries.sql
@@ -1,25 +1,25 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * FROM vet_clinic
+SELECT * FROM animals
 WHERE name LIKE '%mon';
 
-SELECT name FROM vet_clinic
-WHERE date_of_birth between '2016-01-01' and '2019-01-01';
+SELECT name FROM animals
+WHERE date_of_birth between '2016-01-01' and '2019-12-31';
 
-SELECT name FROM vet_clinic
+SELECT name FROM animals
 WHERE neutered = TRUE and escape_attempts < 3;
 
-SELECT date_of_birth FROM vet_clinic
+SELECT date_of_birth FROM animals
 WHERE name = 'Agumon' or name = 'Pikachu';
 
-SELECT name, escape_attempts FROM vet_clinic
+SELECT name, escape_attempts FROM animals
 WHERE weight_kg > 10.5;
 
-SELECT * FROM vet_clinic
+SELECT * FROM animals
 WHERE neutered = TRUE;
 
-SELECT * FROM vet_clinic
+SELECT * FROM animals
 WHERE name != 'Gabumon';
 
-SELECT * FROM vet_clinic
+SELECT * FROM animals
 WHERE weight_kg >= 10.4 and weight_kg <= 17.3;
