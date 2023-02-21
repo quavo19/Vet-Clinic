edb0481a44f72238e84fc1657540feec62bd11c5
Donald Akite <donaldakite27@gmail.com>
2023-02-21 14:23:54 +0000

    Update insertion location

--- data.sql
+++ data.sql
@@ -1,13 +1,13 @@
 /* Populate database with sample data. */
 
-INSERT INTO vet_clinic (ID,name,date_of_birth,escape_attempts,neutered,weight_kg)
+INSERT INTO animals (ID,name,date_of_birth,escape_attempts,neutered,weight_kg)
 VALUES (1, 'Agumon', '2020-02-03', 0, TRUE, 10.23);
 
-INSERT INTO vet_clinic (ID,name,date_of_birth,escape_attempts,neutered,weight_kg)
+INSERT INTO animals (ID,name,date_of_birth,escape_attempts,neutered,weight_kg)
 VALUES (2, 'Gabumon', '2018-11-15',  2, TRUE, 8);
 
-INSERT INTO vet_clinic (ID,name,date_of_birth,escape_attempts,neutered,weight_kg)
+INSERT INTO animals (ID,name,date_of_birth,escape_attempts,neutered,weight_kg)
 VALUES (3, 'Pikachu', '2021-01-07',  1, FALSE, 15.04);
 
-INSERT INTO vet_clinic (ID,name,date_of_birth,escape_attempts,neutered,weight_kg)
+INSERT INTO animals (ID,name,date_of_birth,escape_attempts,neutered,weight_kg)
 VALUES (4, 'Devimon', '2017-05-12',  5, TRUE, 11);
